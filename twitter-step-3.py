93d3dbed1ac2e50aa98944fc2b0ba36fdd53a4ff
mohamadaliRahimi <rmohamadali531@gmail.com>
2018-04-14 00:11:42 +0430

    step3

---
+++ twitter-step-3.py
@@ -0,0 +1,24 @@
+import os,fnmatch,codecs
+import re
+
+
+wordcount = {}
+for dirpath, dirs, files in os.walk('nlp/step2'):
+    for filename in fnmatch.filter(files, '*.txt'):
+        with codecs.open(os.path.join(dirpath, filename),'r',encoding="utf-8")as f:
+
+            for word in f.read().split():
+                if word not in wordcount:
+                    wordcount[word] = 1
+                else:
+                    wordcount[word] += 1
+
+files_object = open("nlp/step3/"+"word-frequency"+ ".txt" , "w" ,encoding='utf-8')
+
+for k,v in sorted(wordcount.items(), key=lambda x: x[1] , reverse=True):
+    files_object.write(str(k) + ":" + str(v) + "\n")
+
+files_object.close();
+
+
+
